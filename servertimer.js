849d45667e86f223d11c0405bc554774c4717e61
UnblockedGamesAlien1947 <210449015+WildAlien1947@users.noreply.github.com>
2025-10-26 14:40:00 -0400

    Delete servertimer.js

--- servertimer.js
+++
@@ -1,4 +0,0 @@
-// Set the target date and time when maintenance should end (e.g., 24 hours from now)
-        // You should change this timestamp before deploying!
-        // Format: 'Oct 21, 2025 10:00:00 GMT+00:00'
-        const targetDate = new Date().getTime() + (0 * 60 * 60 * 1000); // Set to 24 hours from now
